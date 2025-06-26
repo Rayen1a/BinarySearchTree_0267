2e62bbafc6091cb5c0a8d37e04e473e55ec10a8e
Rayen1a <raihan01426@gmail.com>
2025-06-26 11:13:31 +0700

    membuat Void seach currentNode & parent

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -78,4 +78,19 @@ public:
             return;
         }
     }
+
+    void search(int element, Node *&parent, Node *&currentNode)
+    {
+        // This function searches the currentNode of the specifield Node as well as the current Node of its parent
+        currentNode = ROOT;
+        parent = nullptr;
+        while ((currentNode != nullptr) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }
+    }
 }
