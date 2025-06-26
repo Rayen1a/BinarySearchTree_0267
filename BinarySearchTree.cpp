c884aa347bfcca4b42dca232d5c800bb3158e630
Rayen1a <raihan01426@gmail.com>
2025-06-26 10:59:51 +0700

    Memasukkan contructor node leftchild & rightchild

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -7,4 +7,11 @@ public:
     int info;
     Node *leftchild;
     Node *rightchild;
-}
+
+    // Construktor for the node class
+    Node()
+    {
+        leftchild = nullptr;  // initialize left child to null
+        rightchild = nullptr; // initialiaze rigt child to null
+    }
+};
